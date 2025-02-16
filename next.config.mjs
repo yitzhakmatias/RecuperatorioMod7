09e8a7cefe24ddfd43c8bb2e62152945bedf445e
yitzhak <matias6@gmail.com>
2025-02-16 02:17:05 -0400

    wrapper was added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',         // Export static files
+    trailingSlash: true,      // Ensure proper URLs with trailing slash (needed for GitHub Pages)
+    reactStrictMode: true,
+};
 
 export default nextConfig;
